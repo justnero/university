b11943be23e96bc98f7df4eb0ee944f43a784bdf
justnero-ru <justnero.ru@yandex.ru>
2016-03-09 22:32:28 +0300

    .NET Lab.02 code

---
+++ semestr.04/.NET/Lab.02/code/Museum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab._02
+{
+    class Museum: AMuseum, IVisitable
+    {
+
+        private string owner;
+        public string Owner
+        {
+            get { return owner; }
+            set { if (owner != "") owner = value; }
+        }
+
+        private int visits = 0;
+        
+        public Museum(string name, string address, string owner): base(name, address) {
+            this.owner = owner;
+        }
+
+        new public void print()
+        {
+            Console.WriteLine("Museum \"{0}\", {1}; Owner: {2}, Visits: {3}", Name, Address, owner, visits);
+            base.printHalls();
+        }
+
+        override public void printInfo()
+        {
+            Console.WriteLine("Owner: {0}, Visits: {1}", owner, visits);
+        }
+
+
+        public void visit()
+        {
+            visits++;
+        }
+
+        public int getVisits()
+        {
+            return visits;
+        }
+    }
+}
---
+++ semestr.04/.NET/Lab.02/code/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tSevSU semestr.04 .NET Lab.02");
+            Museum m = new Museum("Центральный", "ул. Пушкина", "Город");
+            m[0] = "Холл";
+            m[1] = "Зал A";
+            m[2] = "Зал Б";
+            m[3] = "Зал В";
+            m[4] = "Зал Г";
+
+            Random rnd = new Random();
+            for (int i = 0; i < rnd.Next(100); i++)
+            {
+                m.visit();
+            }
+            try
+            {
+                m.Name = "Областной";
+            }
+            catch (InvalidValueException ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+            m.print();
+            Console.WriteLine("\tPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
