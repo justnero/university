4a028212d48aa88bfe1e398c9de4e27ed02f3efd
justnero-ru <justnero.ru@yandex.ru>
2016-03-16 23:41:39 +0300

    EMS Lab.02 done

--- semestr.04/EMS/Lab02/main.asm
+++ semestr.04/EMS/Lab02/main.asm
@@ -6,35 +6,49 @@
 .def count=r20
 
 init: 
-	ldi count, 7
-	ldi a, 10
-	ldi b, 30
-	ldi r21, $FF
-	ldi r22, $55
-	ldi r23, $AA
+	ldi a, 1			; Запись первого числа
+	ldi b, 2			; Запись второго числа
+	ldi r21, $FF		; Константа для вывода в случае переполнения
+	ldi r22, $55		; Константа для мигания в случае удачи
+	ldi r23, $AA		; Константа для мигания в случае удачи
+	ldi r24, $00		; Константа для обнуления
 	jmp main
 
-for1:
-	add d, c
-	brbs SREG_V, blink
+mult:
+	ldi count, 7		; Множитель/количество итераций сложения
+mult_for:
+	adc d, c
+	brcs fail			; Если произошел перенос в процессе умножения	
 	dec count
-	brne for1
+	brne mult_for		; Цикл for на count итераций
+	jmp success			; Когда цикл закончился перейти к помигиванию
+	
+fail:					; Вывод $FF если переполнение
+	ldi count, 32		; Цикл на 32 помигивания
 	out PORTB, r21		; Вывод на порт B $FF
-	ret
-blink:
-	ldi count, 32 		; Цикл на 32 помигивания
-blinkfor:
-	out PORTD, r22		; Вывод на порт D $55
+fail_for:
 	nop
+	dec count
+	brne fail_for
+	out PORTB, r24		; Обнулить вывод в порт B
+	ret
+	
+success:				; Помигивание если переполнения не было
+	ldi count, 32 		; Цикл на 32 помигивания
+success_for:
+	out PORTD, r22		; Вывод на порт D $55
+	nop	
 	out PORTD, r23		; Вывод на порт D $AA
 	nop
 	dec count
-	brne blinkfor
+	brne success_for	; Цикл for на count итераций
+	out PORTD, r24		; Обнулить вывод в порт D
 	ret
 
 main:
 	mov c, a
-	add c, b
+	adc c, b
+	brcs fail			; Если произошел перенос при первичном сложении
 	ldi d, 0
-	call for1
+	call mult
 	
