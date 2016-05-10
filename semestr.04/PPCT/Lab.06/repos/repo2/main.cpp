eee472abbd87164e422e5f686f783d0ffad47dcf
justnero-ru <justnero.ru@yandex.ru>
2016-05-11 00:46:16 +0300

    PPCT Lab.06 done

---
+++ semestr.04/PPCT/Lab.06/repos/repo2/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+	// I think we need some extra info here
+	cout << "Hello, Hydrargyrum!" << endl;
+	cout << "We edit this file now..." << endl;
+	return 0;
+}
