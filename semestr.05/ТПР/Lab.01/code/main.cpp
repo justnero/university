5b00ea1f0b04c900f351c3569718c469a236af94
justnero-ru <justnero.ru@yandex.ru>
2016-09-21 19:03:28 +0300

    ТПР фикс кода

--- semestr.05/ТПР/Lab.01/code/main.cpp
+++ semestr.05/ТПР/Lab.01/code/main.cpp
@@ -16,7 +16,7 @@ void dot(bool **m, int n) {
     bool isEmpty = true;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
-            if(m[i][j] && i != j) {
+            if (m[i][j] && i != j) {
                 dotos << "\t\"x" << i << "\"" << " -> " << "\"x" << j << "\"" << endl;
                 isEmpty = false;
             }
@@ -25,17 +25,17 @@ void dot(bool **m, int n) {
     dotos << "}" << endl;
     dotos.close();
 
-    if(!isEmpty) {
+    if (!isEmpty) {
         sprintf(str, "dot graph-%d.dot -Tpng -O", k);
         system(str);
     }
     sprintf(str, "rm -f graph-%d.dot", k);
-     system(str);
+    system(str);
     k++;
 }
 
 void draw(bool **m, int n) {
-    dot(m,n);
+    dot(m, n);
     os << "== MATRIX ==" << endl;
     for (int i = 1; i <= n; i++) {
         for (int j = 1; j <= n; j++) {
@@ -58,6 +58,62 @@ void draw(bool **m, int n) {
     os << "============" << endl;
 }
 
+void run_level_up(vector<int> &level, bool **m, int n) {
+    level.clear();
+    for (int i = 1; i <= n; i++) {
+        if (m[i][i]) {
+            continue;
+        }
+        bool isEmpty = true;
+        for (int j = 1; j <= n && isEmpty; j++) {
+            if (m[j][i]) {
+                isEmpty = false;
+            }
+        }
+        if (isEmpty) {
+            level.push_back(i);
+        }
+    }
+}
+
+void run_level_down(vector<int> &level, bool **m, int n) {
+    vector<int> tmp;
+    vector<bool> isDests;
+    for (int i = 1; i <= n; i++) {
+        if (m[i][i]) {
+            continue;
+        }
+        bool isEmpty = true;
+        for (int j = 1; j <= n && isEmpty; j++) {
+            if (m[i][j] && !m[i][i] && !m[j][j]) {
+                isEmpty = false;
+            }
+        }
+        if (isEmpty) {
+            bool isDest = false;
+            for (int j = 1; j <= n && !isDest; j++) {
+                if (m[j][i]) {
+                    isDest = true;
+                }
+            }
+            isDests.push_back(isDest);
+            tmp.push_back(i);
+        }
+    }
+    bool onlyFalse = true;
+    for(int i=0;i<isDests.size() && onlyFalse;i++) {
+        if(isDests[i]) {
+            onlyFalse = false;
+        }
+    }
+    level.clear();
+    for(int i=0;i<tmp.size();i++) {
+        if(onlyFalse || isDests[i]) {
+            level.push_back(tmp[i]);
+        }
+    }
+}
+
 int main() {
 
     char dir;
@@ -78,26 +134,13 @@ int main() {
     os << "Initial" << endl;
     draw(m, n);
 
+    void (*run_level)(vector<int> &level, bool **m, int n) = dir == 'u' ? &run_level_up : &run_level_down;
+
     bool isEnded = false;
     vector<int> level;
     int lvl = 1;
     while (!isEnded) {
-        level.clear();
-        for (int i = 1; i <= n; i++) {
-            if (m[i][i]) {
-                continue;
-            }
-            bool isEmpty = true;
-            for (int j = 1; j <= n && isEmpty; j++) {
-                if ((dir == 'd' && m[i][j] && !m[i][i] && !m[j][j]) ||
-                    (dir == 'u' && m[j][i])) {
-                    isEmpty = false;
-                }
-            }
-            if (isEmpty) {
-                level.push_back(i);
-            }
-        }
+        run_level(level, m, n);
         if (level.size()) {
             os << "Level " << lvl++ << ": ";
             for (vector<int>::iterator it = level.begin(); it != level.end(); it++) {
