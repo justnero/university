c38cafc37fd2457248dc8ac47be037ec360ad8a5
justnero-ru <justnero.ru@yandex.ru>
2016-10-13 20:44:38 +0300

    Лабы по ИСИС и ТПР, дополнение к списку

--- semestr.05/ТПР/Lab.02/indiff.m
+++ semestr.05/ТПР/Lab.02/indiff.m
@@ -3,7 +3,7 @@ function indiff(K1, K2)
     b = zeros(4);
     d = zeros(4, 100);
     e = zeros(4, 100);
-    for i=2:4
+    for i=1:4
         for j=0:i-1
             a(i, j+1) = K1(j+1);
             b(i, j+1) = K2(i-j);
--- semestr.05/ТПР/Lab.02/run.m
+++ semestr.05/ТПР/Lab.02/run.m
@@ -1,8 +1,10 @@
+clear;
+clc;
 x = [30 50 80 25;
      45 30 20 55];
-v1 = 25:25:100;
-v2 = 20:20:80;
-ud = 0:3;
+v1 = 25:7.5:100;
+v2 = 20:6:80;
+ud = 0:10;
 
 k1 = sort(1./v1);
 k2 = sort(1./v2);
@@ -13,11 +15,16 @@ u = [0 0 0 0];
 imax = 0;
 for i = 1:4
     u(i) = polyval(u1,1/x(1,i))*j + polyval(u2,1/x(2,i))*(1-j);
+    fprintf('\tVariant %d:\n', i);
+    fprintf('Price: %d\n', x(1, i));
+    fprintf('Mileage: %d\n', x(2, i));
+    fprintf('Utility %.6f\n', u(i));
     if imax == 0 || u(i) > u(imax) 
         imax = i;
     end
 end
 
+fprintf('\n');
 fprintf('\tBest:\n');
 fprintf('Price: %d\n', x(1, imax));
 fprintf('Mileage: %d\n', x(2, imax));
