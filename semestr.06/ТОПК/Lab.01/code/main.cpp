c745302cfb5c080bf8f3fcf2e23f6e0dec7f24e0
JustNero <justnero.ru@yandex.ru>
2017-02-16 14:13:38 +0300

    ТОПК 1 лаба фикс

--- semestr.06/ТОПК/Lab.01/code/main.cpp
+++ semestr.06/ТОПК/Lab.01/code/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 
-const int regex[3][15] = {
-        {-1, 10, 3, 4, -1, 4,  -1, -1, 7,  9,  -1, 10, 13, 9,  9},
-        {1,  2,  3, 2, 5,  -1, -1, 8,  -1, -1, 11, -1, -1, 5,  -1},
-        {-1, 14, 3, 6, -1, 6,  6,  -1, 9,  -1, -1, 12, 5,  -1, 6}
+const int regex[3][13] = {
+        {-1, 8, 3, 4, -1, 4,  -1, 7,  -1, 8, 11, 7,  7},
+        {1,  2,  3, 2, 5,  -1, -1, -1, 9, -1, -1, 5,  -1},
+        {-1, 12, 3, 6, -1, 6,  6,  -1, -1, 10, 5,  -1, 6}
 };
 
-bool validate(const char *filename, int &cnt) {
+int validate(const char *filename, int &cnt) {
     FILE *file = fopen(filename, "r");
     int c, i = 0;
     cnt = 0;
@@ -34,12 +34,13 @@ bool validate(const char *filename, int &cnt) {
     printf("\"\nState = %d\n", i);
     fclose(file);
     // Конечные состояния – являются допустимым концом
-    return (i == 6 || i == 9 || i == 13 || i == 14);
+    return (i == -1 || i == 6 || i == 7 || i == 8 || i == 10 || i == 11 || i == 12) ? i : -2;
 }
 
 int main(int argc, char **argv) {
     int cnt;
-    printf("%s on length %d", validate("input.txt", cnt) ? "Valid" : "Invalid", cnt);
+    int res = validate("input.txt", cnt);
+    printf("%s on length %d", res == -2 ? "Not full" : res == -1 ? "Invalid" : "Valid", cnt);
 
     return 0;
 }
